e6eef1a77016d7622c12945d951ecb409146b4d7
Ryan <131712164+ryanNS3@users.noreply.github.com>
2023-10-02 17:00:15 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express") //Importa o Express
+const app = express() // Importa o servidor do Express
+const dotenv = require("dotenv") 
+const conectar_bd = require('./service/mongo_connect')
+const cors = require('cors')
+
+dotenv.config() // configurando as variaveis de ambiente
+
+//Configurando os middleware
+
+app.use(cors()) // ativando o cross origem resourse share para a api aceitar multiplos servidores enviando e recebendo dados.
+conectar_bd()
+
+//Rotas
+
+//Middleware caso não ache a rota
+
+app.use((req,res,next) => {res.status(404).json("Recurso não encontrado")})
+
+
+app.listen(process.env.SERVER_PORT, console.log("Servidor aberto na porta: " + process.env.SERVER_PORT)) // Liga o servidor do express
+
